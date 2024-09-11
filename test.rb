f86cdba28a1c774452d36f8250c42438ddb1823e
Matt Yeldalo <matman370@hotmail.com>
2024-09-11 21:28:29 +1000

    stuff

--- test.rb
+++ test.rb
@@ -5,7 +5,7 @@ class QuickBench
   #
   # Compares two methods, see Benchmark.measure to learn about the output
   #
-  # @param [Array] method_names The list of names for method_a and method_b
+  # @param [Array] method_names The list of names. E.g. `['method_a', 'method_b']`
   # @param [Proc] a_lambda The first method to test
   # @param [Proc] b_lambda The second method to test
   def self.compare_methods(method_names, a_lambda, b_lambda)
@@ -33,6 +33,7 @@ class QuickBench
                   ])
   end
 
+  # Hide method by putting it on the class, not the same as 'private'
   class << self
     def wrap_with_box(inner_contents)
       puts ' ----------------------------------------------'
@@ -47,8 +48,16 @@ class QuickBench
 end
 
 str = 'X' * 1024 * 1024 * 10 # 10MB String
-str1 = 'X' * 1024 * 1024 * 10 * 10
+str1 = 'X' * 1024 * 1024 * 10 * 10 * 10
 
-QuickBench.compare_methods(['downcase', 'downcase!'], -> { str.downcase }, -> { str.downcase! })
+stra = 'X' * 1024 * 1024 * 10 # 10MB String
+strb = 'X' * 1024 * 1024 * 10 * 10 * 10
+
+# QuickBench.compare_methods(['downcase', 'downcase!'], -> { str.downcase }, -> { str.downcase! })
 
 QuickBench.compare_methods(['downcase', 'downcase!'], -> { str1.downcase }, -> { str1.downcase! })
+
+# QuickBench.compare_methods(['upcase', 'upcase!'], -> { stra.upcase }, -> { stra.upcase! })
+
+QuickBench.compare_methods(['upcase', 'upcase!'], -> { strb.upcase }, -> { strb.upcase! })
+
